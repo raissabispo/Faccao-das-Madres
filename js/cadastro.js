1e08c1083d7f8479bd10f19589b531bfc6c98647
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2025-02-25 21:24:20 +0000

    Adicionando a tela de cadastro

---
+++ js/cadastro.js
@@ -0,0 +1,29 @@
+function enviar(event) {
+    event.preventDefault(); 
+  
+    let email = document.getElementById('email').value.trim();
+    let senha = document.getElementById('senha').value.trim();
+  
+    let emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email === '' || senha === '') {
+        alert('Por favor, preencha todos os campos obrigatórios!');
+        return false;
+    } 
+    
+    if (!emailValido.test(email)) {
+        alert('Por favor, insira um e-mail válido!');
+        return false;
+    }
+  
+    alert('Cadastro realizado com sucesso!'); 
+    window.location.href = "login.html";
+    
+
+    return true;
+}
+
+document.getElementById('senhaMostrar').addEventListener('change', function () {
+    let senhaInput = document.getElementById('senha');
+    senhaInput.type = this.checked ? 'text' : 'password';
+});
